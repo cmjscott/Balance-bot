69b7405d85a69e9ebfa0182204d7c783bd9181fc
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-11-26 21:57:17 -0500

    Output data

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -14,8 +14,16 @@ void setup()
 void loop()
 {
 	Adafruit9DOF.update();
-	Serial.print(Adafruit9DOF.get_roll());  Serial.print('\t');
+/*	Serial.print(Adafruit9DOF.get_roll());  Serial.print('\t');
 	Serial.print(Adafruit9DOF.get_pitch()); Serial.print('\t');
 	Serial.print(Adafruit9DOF.get_yaw());   Serial.print('\n');
-	delay(5);
+	delay(5); //*/
+	Serial.print(micros()); Serial.print(',');
+	Serial.print(Adafruit9DOF.get_lin_accel().x, 6), Serial.print(',');
+	Serial.print(Adafruit9DOF.get_lin_accel().y, 6), Serial.print(',');
+	Serial.print(Adafruit9DOF.get_lin_accel().z, 6), Serial.print(',');
+	Serial.print(Adafruit9DOF.get_ang_vel().x, 6), Serial.print(',');
+	Serial.print(Adafruit9DOF.get_ang_vel().y, 6), Serial.print(',');
+	Serial.print(Adafruit9DOF.get_ang_vel().z, 6), Serial.print('\n');
+	delay(5); //*/
 }
